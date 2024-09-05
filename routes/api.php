79988d158e9369848e49d1a20896430f179a7c8e
sojostar <juansojo@gmail.com>
2024-09-05 14:30:35 -0400

    puse un espacio de prueba

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/random/{min}/{max}', function ($min, $max) {
     $number_random = rand($min,$max);
     $array = array('numero' => $number_random);
-    return response($array,200);
+    return response($array,200); 
 });
