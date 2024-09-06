11f288e1d60bfb9e42ab08d6eac21e64c56bc390
Sojostar <juansojo@gmail.com>
2024-09-06 07:59:35 -0400

    usando_migracion_discapacidad

---
+++ app/Models/generico/Discapacidad.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\generico;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Discapacidad extends Model
+{
+    use HasFactory;
+    protected $table = 'generico.discapacidad';
+}
---
+++ app/Models/generico/Sectores.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\generico;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sectores extends Model
+{
+    use HasFactory;
+}
