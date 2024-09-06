c79b4faca588626bc8b4e3e8a26e27858c1118e8
Sojostar <juansojo@gmail.com>
2024-09-06 06:31:47 -0400

    menu de propietarios

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('propietarios.ver')" :active="request()->routeIs('propietarios.ver')">
+                        {{ __('Owners') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::middleware('auth')->group(function () {
+    Route::get('/propietarios', [PropietariosController::class, 'edit'])->name('propietarios.ver');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
